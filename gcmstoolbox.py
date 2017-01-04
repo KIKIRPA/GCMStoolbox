0827472fb829dec3e5e27d0e96b84b2546fb4475
kobbejager <wfremout+github@gmail.com>
2017-01-04 13:17:32 +0100

    read ELU files, part 1

--- gcmstoolbox.py
+++ gcmstoolbox.py
@@ -10,11 +10,17 @@ def main():
 
 
 
-def readspectrum(fh, i = 0, verbose = False, match = []):
+def readspectrum(fh, i = 0, match = [], norm = False, elu = False, eluAll = False, verbose = False):
   # we expect that each spectrum starts with 'name' (case insensitive)
   # we use this as a trigger to start recording the metadata, reading the filehandle line by line
   # once we read 'num peaks' we start collecting the spectrum itself, counting the number
   # once numpeaks is reached, we return the data as a dictonary
+  
+  # TODO:
+  #  1.  normalise when norm var is true
+  #  2.  decompose ELU name
+  #  3.  eluAll == False => OR1
+  #  4.  change readspectrum arguments in scripts
   
   for line in fh: 
     if line.casefold().startswith('name'):
@@ -23,6 +29,11 @@ def readspectrum(fh, i = 0, verbose = False, match = []):
       readmeta = True
       readdata = False
       xy = {}
+      
+      if elu:
+        eluNameParts = spectrum['Name'].split('|')
+        for p in eluNameParts:
+          if p.startswith('RI'): 
       
       #match: list of spectrum names: only return spectrum for those, "no match" for others
       if (len(match) > 0) and (spectrum["Name"] not in match):
@@ -44,52 +55,54 @@ def readspectrum(fh, i = 0, verbose = False, match = []):
           spectrum['Num Peaks'] = numpeaks
           readmeta = False
           readdata = True
-        
-        elif nextline.casefold().startswith('cas#'):
-          #NOTE: NIST seems to store sometimes CAS# and NIST# on the same line, CAS# first and then NIST# 
-          #      separated with semicolon. I haven't seen AMDIS doing this. I hope this is the only case?
-          if 'nist#' in nextline.casefold():
-            parts = nextline.split(';', 1)
-            spectrum["CAS#"]  = parts[0].split(':', 1)[1].strip()
-            spectrum["NIST#"] = parts[1].split(':', 1)[1].strip()
-          else:
-            spectrum["CAS#"]  = nextline.split(':', 1)[1].strip()
+          
+        elif not elu: #all metadata in ELU files is contained within the name string!
+          if nextline.casefold().startswith('cas#') and not elu:
+            #NOTE: NIST seems to store sometimes CAS# and NIST# on the same line, CAS# first and then NIST# 
+            #      separated with semicolon. I haven't seen AMDIS doing this. I hope this is the only case?
+            if 'nist#' in nextline.casefold():
+              parts = nextline.split(';', 1)
+              spectrum["CAS#"]  = parts[0].split(':', 1)[1].strip()
+              spectrum["NIST#"] = parts[1].split(':', 1)[1].strip()
+            else:
+              spectrum["CAS#"]  = nextline.split(':', 1)[1].strip()
       
-        elif readmeta:     #all metadata
-          #NOTE: I assume that each metadata field is restrained to a single line; I haven't seen any
-          #      multiline field so far. This code only supports single lines!
-          #NOTE: We don't support multiple "Synon" tags as in the NIST MSP files
-          #      (if we would implement it, we need to put those in a list/array)
-          parts = nextline.split(':', 1)
-          parts[0] = parts[0].strip().title()       # field name: each first letter is capilalised in NIST
-          if parts[0] == "Casno": parts[0] = "CAS#" # exception: CASNO in Amdis translates into CAS# in NIST
-          if parts[0] == "Nist#": parts[0] = "NIST#"
-          if parts[0] == "Db#":   parts[0] = "DB#"
-          if parts[0] == "Ri":    parts[0] = "RI"
-          if parts[0] == "Rt":    parts[0] = "RT"
-          if parts[0] == "Mw":    parts[0] = "MW"
-          if parts[0] == "Exactmass": parts[0] = "ExactMass"
-          spectrum[parts[0]] = parts[1].strip()
+          if readmeta:     #all metadata
+            #NOTE: I assume that each metadata field is restrained to a single line; I haven't seen any
+            #      multiline field so far. This code only supports single lines!
+            #NOTE: We don't support multiple "Synon" tags as in the NIST MSP files
+            #      (if we would implement it, we need to put those in a list/array)
+            parts = nextline.split(':', 1)
+            parts[0] = parts[0].strip().title()       # field name: each first letter is capilalised in NIST
+            if parts[0] == "Casno": parts[0] = "CAS#" # exception: CASNO in Amdis translates into CAS# in NIST
+            if parts[0] == "Nist#": parts[0] = "NIST#"
+            if parts[0] == "Db#":   parts[0] = "DB#"
+            if parts[0] == "Ri":    parts[0] = "RI"
+            if parts[0] == "Rt":    parts[0] = "RT"
+            if parts[0] == "Mw":    parts[0] = "MW"
+            if parts[0] == "Exactmass": parts[0] = "ExactMass"
+            spectrum[parts[0]] = parts[1].strip()
         
         elif readdata:    # read spectral data
           #rough conversion from Amdis bracket-style to NIST semicolon style
-          nextline = nextline.replace("(", "").replace(")", ";")
+          nextline = nextline.replace("(", "").replace(")", ";").replace(",", " ")
           
           #prepare for splitting
           if nextline[-1:] == ";":      #remove ; from the end of the line (if present)
             nextline = nextline[:-1]
           
           #split into X-Y couples, separate values and append to x and y series
+          #ELU files might have something extra, which we will discard
           couples = nextline.split(";")
           for couple in couples:
-            x, y = couple.split(None, 1)   #None should split on multiple whitespaces
-            xy[int(x.strip())] = int(y.strip())
+            couple = couple.split(None)   #None should split on multiple whitespaces
+            xy[int(couple[0].strip())] = int(couple[1].strip())
           
           #countdown numpeaks and prepare to end this function
           numpeaks = numpeaks - len(couples)
           if numpeaks == 0:
             #one more thing: is y normalised?
-            #Amdis normalises to 1000, NIST to 999
+            #NIST normalises to 999, AMDIS seems to normalise in most cases to the highest _certain_ peak (999)
             #if normalised to 1000, just change this value(s) to 999 (very small error)
             #if not normalised, we won't touch it
             if max(xy.values()) == 1000:
